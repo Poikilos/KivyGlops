364bce282d98caeb0873eadc18661621eb31230e
poikilos <7557867+poikilos@users.noreply.github.com>
2022-03-10 15:31:21 -0500

    Add the mastercompare script (from stable).

---
+++ mastercompare.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# meld ./ ~/git/KivyGlops
+meld kivyglops.py ../KivyGlops/kivyglops/__init__.py &
+meld pyglops.py ../KivyGlops/kivyglops/pyglops.py &
+meld pyrealtime.py ../KivyGlops/kivyglops/pyrealtime.py &
+
+# For staging, run compare-staging.sh instead.
