0375bd48df03bcb7fe53f4e192c270580e287df4
poikilos <7557867+poikilos@users.noreply.github.com>
2022-03-10 15:03:12 -0500

    Improve comparison scripts.

--- compare-staging.sh
+++ compare-staging.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-meld ./ ~/git/KivyGlops-audit-3a66898
+# meld ./ ~/git/KivyGlops-audit-3a66898
+STAGING_DIR="KivyGlops-audit-3a66898"
+meld ../$STAGING_DIR/kivyglops.py ../KivyGlops/kivyglops/__init__.py &
+meld ../$STAGING_DIR/pyglops.py ../KivyGlops/kivyglops/pyglops.py &
+meld ../$STAGING_DIR/pyrealtime.py ../KivyGlops/kivyglops/pyrealtime.py &
+meld ../$STAGING_DIR/wobjfile.py ../KivyGlops/kivyglops/wobjfile.py &
--- mastercompare.sh
+++ mastercompare.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-meld ./ ~/git/KivyGlops
+# meld ./ ~/git/KivyGlops
+meld kivyglops.py ../KivyGlops/kivyglops/__init__.py &
+meld pyglops.py ../KivyGlops/kivyglops/pyglops.py &
+meld pyrealtime.py ../KivyGlops/kivyglops/pyrealtime.py &
+
+# For staging, see compare-staging.sh.
