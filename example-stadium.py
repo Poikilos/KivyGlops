27afcfc10c73418aa405cc7ed244b9cf23c7bbc3
poikilos <7557867+poikilos@users.noreply.github.com>
2022-03-13 19:30:04 -0400

    From master: Use PEP8 more; Use single quotes for keys more consistently; Use platform-specific slashes.

--- example-stadium.py
+++ example-stadium.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+import math
+import os
 
 from kivy.app import App
 from kivy.core.window import Window
@@ -7,12 +10,12 @@ from kivy.logger import Logger
 from kivy.vector import Vector
 from kivy.core.image import Image
 from kivy.core.window import Keyboard
-#from kivy.graphics.transformation import Matrix
-#from kivy.graphics.opengl import *
-#from kivy.graphics import *
-#from objloader import ObjFile
-#from pyrealtime import *
-#from kivy.clock import Clock
+# from kivy.graphics.transformation import Matrix
+# from kivy.graphics.opengl import *
+# from kivy.graphics import *
+# from objloader import ObjFile
+# from pyrealtime import *
+# from kivy.clock import Clock
 from kivy.input.providers.mouse import MouseMotionEvent
 from kivy.factory import Factory
 from kivy.uix.boxlayout import BoxLayout
@@ -20,8 +23,6 @@ from kivyglops import *
 from common import (
     set_verbose_enable,
 )
-import math
-import os
 
 profile_path = None
 if 'USERPROFILE' in os.environ:  # if os_name=="windows":
@@ -45,37 +46,52 @@ class MainScene(KivyGlops):
             if result is not None:
                 break
 
-        walkmesh_names = self.get_similar_names("walkmesh")
-        for name in walkmesh_names:
-            print("Found possible walkmesh: "+name)
-            is_ok = self.use_walkmesh(name, hide=True)
+        walkmesh_enable = True
+        if walkmesh_enable:
+            walkmesh_names = self.get_similar_names("walkmesh")
+            for name in walkmesh_names:
+                print("Found possible walkmesh: "+name)
+                is_ok = self.use_walkmesh(name, hide=True)
+        else:
+            print("[ example-stadium ] WARNING: not using walkmesh")
 
-        #self.load_obj("player1.obj")
+        # self.load_obj("player1.obj")
 
-        item_dict = dict()
+        item_dict = {}
         item_dict['name'] = "rock"
-        item_dict["bump"] = "hide; obtain"
-        item_dict["uses"] = ["throw_arc", "melee"]
-        item_dict["target_types"] = ["surface", "actor", "glop"]  # TODO
-        item_dict["cooldown"] = .7
+        item_dict['bump'] = "hide; obtain"
+        item_dict['uses'] = ['throw_arc', 'melee']
+        item_dict['target_types'] = ["surface", "actor", "glop"]  # TODO
+        item_dict['cooldown'] = .7
         item_dict['hit_damage'] = .3
         item_dict['projectile_keys'] = ['hit_damage']
 
-        #weapon = dict()
-        #weapon['hit_damage'] = .3
+        # weapon = {}
+        # weapon['hit_damage'] = .3
 
-        #item_dict['as_projectile'] = weapon
-
+        # item_dict['as_projectile'] = weapon
+        carpeted_cement_hit_light1 = os.path.join(
+            "sounds",
+            "carpeted-cement-hit,light1.wav"
+        )
+        carpeted_cement_hit1 = os.path.join(
+            "sounds",
+            "carpeted-cement-hit1.wav"
+        )
+        carpeted_cement_hit2 = os.path.join(
+            "sounds",
+            "carpeted-cement-hit2.wav"
+        )
         for index in self.get_indices_of_similar_names("rock"):
             print("Preparing item at index " + str(index))
             self.set_as_item_at(index, item_dict)
-            self.add_bump_sound_at(index, "sounds/carpeted-cement-hit,light1.wav")
-            self.add_bump_sound_at(index, "sounds/carpeted-cement-hit1.wav")
-            self.add_bump_sound_at(index, "sounds/carpeted-cement-hit2.wav")
+            self.add_bump_sound_at(index, carpeted_cement_hit_light1)
+            self.add_bump_sound_at(index, carpeted_cement_hit1)
+            self.add_bump_sound_at(index, carpeted_cement_hit2)
 
         self.set_background_cylmap("maps/sky-texture-seamless.jpg")
 
-        human_info = dict()
+        human_info = {}
         human_info['hp'] = 1.0
 
         player1_index = self.get_player_glop_index(1)
@@ -85,38 +101,44 @@ class MainScene(KivyGlops):
         self.add_damaged_sound_at(player1_index, "sounds/body-hit3.wav")
         self.add_damaged_sound_at(player1_index, "sounds/body-hit4.wav")
 
-        chimp_info = dict()
+        chimp_info = {}
         chimp_info['hp'] = 1.0
 
-        chimp_info["land_units_per_second"] = self.glops[player1_index].actor_dict["land_units_per_second"] / 2.
-        # If the ranges below are not set, PyGlops will set defaults for them:
-        chimp_info["ranges"] = {}
-        chimp_info["ranges"]["melee"] = 0.5
-        chimp_info["ranges"]["throw_arc"] = 10.
+        chimp_info['land_units_per_second'] = \
+            self.glops[player1_index].actor_dict['land_units_per_second'] / 2.
+        # If the ranges below are not set, PyGlops will set defaults for
+        # them:
+        chimp_info['ranges'] = {}
+        chimp_info['ranges']['melee'] = 0.5
+        chimp_info['ranges']['throw_arc'] = 10.
 
         enemy_indices = self.get_indices_of_similar_names("chimp")
-        print("Found "+str(len(enemy_indices))+" chimp(s)")
-        for i in range(0,len(enemy_indices)):
+        print("Found {} chimp(s) by similar name"
+              "".format(len(enemy_indices)))
+        for i in range(0, len(enemy_indices)):
             index = enemy_indices[i]
             self.set_as_actor_at(index, chimp_info)
             self.add_damaged_sound_at(index, "sounds/chimp-ooh1.wav")
             self.add_damaged_sound_at(index, "sounds/chimp-upset1.wav")
 
-    def on_attacked_glop(self, attacked_index, attacker_index, weapon_dict):
-        self.glops[attacked_index].actor_dict['hp'] -= weapon_dict['hit_damage']
+    def on_attacked_glop(self, attacked_index, attacker_index,
+                         weapon_dict):
+        self.glops[attacked_index].actor_dict['hp'] -= \
+            weapon_dict['hit_damage']
         if self.glops[attacked_index].actor_dict['hp'] <= 0:
             self.explode_glop_at(attacked_index, weapon_dict)
-        #if get_verbose_enable():
-        print(str(self.glops[attacked_index].name) + "'s hp: " + \
-              str(self.glops[attacked_index].actor_dict['hp']))
+        # if get_verbose_enable():
+        print("{}'s hp: {}"
+              "".format(self.glops[attacked_index].name,
+                        self.glops[attacked_index].actor_dict['hp']))
 
     def on_obtain_glop(self, bumpable_index, bumper_index):
-        #if self.glops[bumpable_index].item_dict['name'] == "rock":
-        #    self.play_sound("sounds/rock-pickup.wav")
+        # if self.glops[bumpable_index].item_dict['name'] == "rock":
+        #     self.play_sound("sounds/rock-pickup.wav")
         pass
 
-    #def on_update_glops(self):
-    #    pass
+    # def on_update_glops(self):
+    #     pass
 
 set_verbose_enable(True)
 scene = MainScene(KivyGlopsWindow())
@@ -125,15 +147,16 @@ scene = MainScene(KivyGlopsWindow())
 class KivyGlopsExampleApp(App):
     def build(self):
         return scene.ui
-        #mainform = MainForm()
-        #boxlayout = TextForm()
-        #boxlayout.add_widget(mainform)
-        #boxlayout.cols = 1
-        #boxlayout.orientation = "vertical"
-        #boxlayout.useButton = Factory.Button(text="Use", id="useButton", \
-        #                                     size_hint=(.1,.1))
-        #boxlayout.add_widget(boxlayout.useButton)
-        #return boxlayout
+        # mainform = MainForm()
+        # boxlayout = TextForm()
+        # boxlayout.add_widget(mainform)
+        # boxlayout.cols = 1
+        # boxlayout.orientation = "vertical"
+        # boxlayout.useButton = Factory.Button(text="Use",
+        #                                      id="useButton",
+        #                                      size_hint=(.1,.1))
+        # boxlayout.add_widget(boxlayout.useButton)
+        # return boxlayout
 
 
 if __name__ == "__main__":
