620153ee99954146dcee37ed2a3d70796ed2672b
poikilos <7557867+poikilos@users.noreply.github.com>
2022-03-14 12:03:10 -0400

    Remove the compare script (use master's instead).

--- mastercompare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# meld ./ ~/git/KivyGlops
-meld kivyglops.py ../KivyGlops/kivyglops/__init__.py &
-meld pyglops.py ../KivyGlops/kivyglops/pyglops.py &
-meld pyrealtime.py ../KivyGlops/kivyglops/pyrealtime.py &
-
-# For staging, run compare-staging.sh instead.
